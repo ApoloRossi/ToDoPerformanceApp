33df3e532617f8a91058ec5b91d55cc589a2906f
Apolo <apolorossi@gmail.com>
2025-12-20 20:03:20 -0300

    feat: Clear text.

--- app/src/main/java/com/apolodevsystem/todoperformance/SearchComponent.kt
+++ app/src/main/java/com/apolodevsystem/todoperformance/SearchComponent.kt
@@ -1,28 +1,22 @@
 package com.apolodevsystem.todoperformance
 
-import android.provider.CalendarContract
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.input.clearText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -70,10 +64,11 @@ fun SimpleSearchBar(
                     ),
                     leadingIcon = {
                         Icon(imageVector = Icons.Default.Search, "")
-
                     },
                     trailingIcon = {
-                        Icon(imageVector = Icons.Default.Clear, "")
+                        Icon(imageVector = Icons.Default.Clear, "", modifier = Modifier.clickable {
+                            textFieldState.clearText()
+                        })
                     },
                     query = textFieldState.text.toString(),
                     onQueryChange = { textFieldState.edit { replace(0, length, it) } },
